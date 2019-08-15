505cdfc91e90d942d92ac6e083761b340e839c37
Alexander Riccio <test35965+learn@gmail.com>
2019-08-15 13:21:32 -0400

    create users model

---
+++ calendar_backend/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    has_secure_password
+    has_many :events, through: :calendar
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :password_digest, presence: true
+
+end
