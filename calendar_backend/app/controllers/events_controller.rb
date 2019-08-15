7bd32091522195df157691fdf86bbb52693e50f2
Harpreet Ghotra <harpreetghotra40@icloud.com>
2019-08-15 16:48:12 -0400

    added routes

--- calendar_backend/app/controllers/events_controller.rb
+++ calendar_backend/app/controllers/events_controller.rb
@@ -1,12 +1,12 @@
 class EventsController < ApplicationController
   def index
-    events = Event.where(:calendar_id => params[:id])
+    events = Event.where(:calendar_id => params[:calendar_id])
     render json: EventSerializer.new(events).to_serialized_json
   end
 
   def create
     updatedTime = DateTime.parse(params[:event_time])
-    event = Event.create!(name: params[:name], description: params[:description], calendar_id: 1, event_time: updatedTime)
+    event = Event.create!(title: params[:title], description: params[:description], calendar_id: 1, event_time: updatedTime)
     render json: EventSerializer.new(event).to_serialized_json
   end
 end
--- calendar_backend/config/routes.rb
+++ calendar_backend/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get "/users/:id", to: "users#show"
+
+  get "/calendars/:id" => "calendars#show"
+
+  #events routes
+  get "/events/:id" => "events#index"
+  post "/events" => "events#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
