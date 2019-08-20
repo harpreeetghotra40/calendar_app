bfb18025ebd12ecb970fa0e6203a5b5c839471b6
Harpreet Ghotra <harpreetghotra40@icloud.com>
2019-08-20 12:37:22 -0400

    form added

--- calendar_frontend/src/components/Signup.js
+++ calendar_frontend/src/components/Signup.js
@@ -1,10 +1,67 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import '../stylesheets/SignUp.css'
 
 
 class Signup extends React.Component {
 
     render() {
-        return (null);
+        return (
+            <div className = "form-container">
+
+                <Form className = "login-form">
+                <h2>Welcome, back</h2>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Login
+                </Button>
+                </Form>
+
+
+                {/* separator */}
+
+                <div className = "separator"><h1>or</h1></div>
+
+
+                <Form className = "signup-form">
+                <h2>Create Account</h2>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter name" />
+                    {/* <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text> */}
+                </Form.Group>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Create Account
+                </Button>
+                </Form>
+          </div>
+        );
     }
 }
 
